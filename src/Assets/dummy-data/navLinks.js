3e766fba8a1d97b907194ed012693bc46d3244c9
Mohammed Ansar <mohammedansarplk@gmail.com>
2024-01-11 13:43:01 +0530

    header added

---
+++ src/Assets/dummy-data/navLinks.js
@@ -0,0 +1,24 @@
+const navLinks = [
+  {
+    path: "/admin/dashboard",
+    icon: "ri-apps-2-line",
+    display: "Dashboard",
+  },
+  {
+    path: "/admin/bookings",
+    icon: "ri-taxi-line",
+    display: "Booking",
+  },
+  {
+    path: "/admin/sell-car",
+    icon: "ri-shopping-bag-line",
+    display: "Sell Cars",
+  },
+  {
+    path: "/admin/settings",
+    icon: "ri-settings-2-line",
+    display: "Settings",
+  },
+];
+
+export default navLinks;
