702b82de04dcfe9d91a76bb327134b30f46c1085
Mohammed Ansar <mohammedansarplk@gmail.com>
2024-01-15 23:06:38 +0530

    added car page

--- src/components/userLayout.jsx
+++ src/components/userLayout.jsx
@@ -1,10 +1,10 @@
 import React, { Fragment } from "react";
 import { Outlet, Link, NavLink } from "react-router-dom";
-import { useAuth } from "../context/AuthContext"
+// import { useAuth } from "../context/AuthContext"
 import Header from "./Header"
 import Footer from "./Footer"
-import styled from "styled-components"
-import Routers from "../routers/UserRouters";
+// import styled from "styled-components"
+// import Routers from "../routers/UserRouters";
 
 const Layout = () => {
   
